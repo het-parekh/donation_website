f940e669ae871fca945e6f2b1e7bb6bce9f772f5
Archit-Patil <58542242+Archit-Patil@users.noreply.github.com>
2020-10-26 19:16:58 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById('login').addEventListener('click', function(){
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.bg-model').style.display = 'none';
+});
