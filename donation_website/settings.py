95708186b4ddff2841bf99dba61e3f1570b363b0
Het <het.parekh@somaiya.edu>
2020-11-06 14:59:32 +0530

    some changes

--- donation_website/settings.py
+++ donation_website/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'user.auth.AuthenticationEmailBackend',
+    'user.backends.AuthenticationEmailBackend',
 ]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
