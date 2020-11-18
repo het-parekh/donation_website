56b8fc72c54feb1b9f93086527a98fdd01e38c9f
Archit-Patil <58542242+Archit-Patil@users.noreply.github.com>
2020-11-18 11:33:31 +0530

    Update urls.py

--- donation_website/urls.py
+++ donation_website/urls.py
@@ -27,6 +27,11 @@ urlpatterns = [
     path('logout/',LogoutView.as_view(template_name="user/logout.html"),name='logout'),
     path('register/',user_views.register,name='register'),
     path('profile/<slug:slug>/',user_views.profile,name='profile'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name="user/password_reset.html"),name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name="user/password_reset_done.html"),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="user/password_reset_confirm.html"),name='password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name="user/password_reset_complete.html"),name='password_reset_complete'),
+    path('activate/<uidb64>/<token>',user_views.VerificationView.as_view(),name='activate'),
     #path('profile/',user_views.profile,name='profile')
     
     
