eeb40dbde13b0755b5ac8d4a3ba680e0eca3c50c
Het <het.parekh@somaiya.edu>
2020-11-27 20:52:42 +0530

    changes settings login redirect and static root

--- donation_website/settings.py
+++ donation_website/settings.py
@@ -134,12 +134,14 @@ USE_TZ = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-LOGIN_URL = 'login' 
-LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'donation-home'
 LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login' 
+LOGOUT_URL = 'logout'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
