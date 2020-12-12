68fed3612c27137b2202bc6d692b67acccfc9dc5
Het <het.parekh@somaiya.edu>
2020-12-12 23:14:13 +0530

    footer removed

--- donation_website/settings.py
+++ donation_website/settings.py
@@ -87,16 +87,16 @@ WSGI_APPLICATION = 'donation_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'NAME': 'db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'testing12345',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'testing12345',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
